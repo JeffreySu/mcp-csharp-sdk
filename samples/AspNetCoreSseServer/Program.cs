343f0e0107e766eeaaad74a5bf0de0fc129d1325
JeffreySu <zsu@senparc.com>
2025-04-08 10:40:38 +0800

    Add dependency injection to McpServerToolType class

--- samples/AspNetCoreSseServer/Program.cs
+++ samples/AspNetCoreSseServer/Program.cs
@@ -1,6 +1,10 @@
+using AspNetCoreSseServer.Tools;
 using TestServerWithHosting.Tools;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<MyData>();
+
 builder.Services.AddMcpServer()
     .WithTools<EchoTool>()
     .WithTools<SampleLlmTool>();
